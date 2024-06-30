6ea40f7ffe12a619d9e4a60d0cd5219c1892a02b
Nicolas Vallee <hello@nicolasvallee.dev>
2024-06-30 12:04:03 +0400

    generate the database

---
+++ GameStore.Api/Data/DataExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Api.Data;
+
+public static class DataExtensions
+{
+  public static void MigrateDb(this WebApplication app)
+  {
+    using var scope = app.Services.CreateScope();
+    var DbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+    DbContext.Database.Migrate();
+  }
+}
